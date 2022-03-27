88dc4f428b16ebb9e6a4b158d350373b19a152d6
wookboy00 <wookboy00@naver.com>
2022-03-27 14:06:01 +0900

    Use cache for config class

--- component/config.py
+++ component/config.py
@@ -1,43 +1,45 @@
 import os
 import json
+from functools import cache
 
 
-class configClass:
-    def __init__(self, filepath, isMainConfigFile=False):
-        self.filepath = filepath
-        self.isMainConfigFile = isMainConfigFile
+class Config:
+    def __init__(self, file_path: str, is_main_config: bool = False):
+        self.file_path = file_path
+        self.is_main_config = is_main_config
 
-    def checkFile(self):
-        print(f'{self.filepath} is checking...')
-        if os.path.isfile(self.filepath):
+    @cache
+    def check_file(self):
+        print(f'{self.file_path} is checking...')
+        if os.path.isfile(self.file_path):
             print('Config file found.')
             return True
         else:
-            if self.isMainConfigFile:
+            if self.is_main_config:
                 if os.path.isfile('./._setting.json'):
                     print(
                         "Default config file has detected. Please rename it to '.setting.json'")
             print(
-                f'*WARNING* Cannot read config file. ({self.filepath}) Please check your config file.')
+                f'*WARNING* Cannot read config file. ({self.file_path}) Please check your config file.')
             return False
 
-    def loadConfig(self):
-        if self.checkFile():
-            with open(self.filepath, 'r') as file:
+    def load_config(self):
+        if self.check_file():
+            with open(self.file_path, 'r') as file:
                 return json.load(file)
         else:
             return False
 
-    def loadConfigPiece(self, piecename):
-        if self.checkFile():
-            with open(self.filepath, 'r') as file:
+    def load_config_piece(self, piecename: str):
+        if self.check_file():
+            with open(self.file_path, 'r') as file:
                 return json.load(file).get(piecename)
         else:
             return False
 
 
-mainConfig = configClass('./.setting.json', True)
-mysqlConfig = mainConfig.loadConfigPiece('mysql')
-tnConfig = mainConfig.loadConfigPiece('twicenest')
-wtConfig = mainConfig.loadConfigPiece('want')
-wkConfig = mainConfig.loadConfigPiece('twicewiki')
+config = Config('./.setting.json', True)
+mysql_config = config.load_config_piece('mysql')
+twicenest_config = config.load_config_piece('twicenest')
+want_config = config.load_config_piece('want')
+wiki_config = config.load_config_piece('twicewiki')
