0aff879379772c3927bf4eb0986fb3fc267bdd38
wookboy00 <wookboy00@naver.com>
2022-09-04 16:59:15 +0900

    Modified want executor log

--- src/executors/want.py
+++ src/executors/want.py
@@ -58,7 +58,7 @@ class Want:
         env = Environment()
         content = self.comment + "\n뮤비 조회수 봇은 TWICEWIKI에 의해 운영되고 관리됩니다.\n\n"
 
-        logger.info("Start to twicenest post process")
+        logger.info("Start to want post process")
         self._change_windowsize(1920, 1080)
 
         self._get_page("https://want.twicenest.com/FREE/login")
